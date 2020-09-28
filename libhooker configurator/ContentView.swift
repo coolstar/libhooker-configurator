8b911a1d246f29ae18c657bc9bfe7f52d5b2c0b1
CoolStar <coolstarorganization@gmail.com>
2020-09-28 16:20:55 -0700

    Initial Commit

---
+++ libhooker configurator/ContentView.swift
@@ -0,0 +1,77 @@
+//
+//  ContentView.swift
+//  libhooker configurator
+//
+//  Created by CoolStar on 9/28/20.
+//  Copyright © 2020 coolstar. All rights reserved.
+//
+
+import SwiftUI
+
+private let dateFormatter: DateFormatter = {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateStyle = .medium
+    dateFormatter.timeStyle = .medium
+    return dateFormatter
+}()
+
+struct ContentView: View {
+    @State private var dates = [Date]()
+
+    var body: some View {
+        NavigationView {
+            MasterView(dates: $dates)
+                .navigationBarTitle(Text("Master"))
+                .navigationBarItems(
+                    leading: EditButton(),
+                    trailing: Button(
+                        action: {
+                            withAnimation { self.dates.insert(Date(), at: 0) }
+                        }
+                    ) {
+                        Image(systemName: "plus")
+                    }
+                )
+            DetailView()
+        }.navigationViewStyle(DoubleColumnNavigationViewStyle())
+    }
+}
+
+struct MasterView: View {
+    @Binding var dates: [Date]
+
+    var body: some View {
+        List {
+            ForEach(dates, id: \.self) { date in
+                NavigationLink(
+                    destination: DetailView(selectedDate: date)
+                ) {
+                    Text("\(date, formatter: dateFormatter)")
+                }
+            }.onDelete { indices in
+                indices.forEach { self.dates.remove(at: $0) }
+            }
+        }
+    }
+}
+
+struct DetailView: View {
+    var selectedDate: Date?
+
+    var body: some View {
+        Group {
+            if selectedDate != nil {
+                Text("\(selectedDate!, formatter: dateFormatter)")
+            } else {
+                Text("Detail view content goes here")
+            }
+        }.navigationBarTitle(Text("Detail"))
+    }
+}
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
